973d2443f190b1521c60e7e5730aa8384668bbf8
Kartik Gupta <rulz.gupta@gmail.com>
2018-04-24 12:55:43 +0530

    Moves retry tests to messaging ns

--- test/ziggurat/mapper_test.clj
+++ test/ziggurat/mapper_test.clj
@@ -1,37 +1,15 @@
 (ns ziggurat.mapper-test
   (:require [clojure.test :refer :all])
-  (:require [ziggurat.mapper :refer [mapper-func]]
-            [lambda-common.metrics :as metrics]
+  (:require [lambda-common.metrics :as metrics]
             [sentry.core :refer [sentry-report]]
-            [ziggurat.config :refer [rabbitmq-config ziggurat-config]]
-            [ziggurat.messaging.connection :refer [connection]]
-            [langohr.basic :as lb]
-            [ziggurat.messaging.producer :as producer]
+            [ziggurat.config :refer [ziggurat-config]]
             [ziggurat.fixtures :as fix]
-            [taoensso.nippy :as nippy]
-            [langohr.channel :as lch]
-            [ziggurat.messaging.consumer :as consumer])
+            [ziggurat.mapper :refer [mapper-func]]
+            [ziggurat.messaging.producer :as producer]
+            [ziggurat.util.rabbitmq :as rmq])
   (:import (java.util Arrays)))
 
 (use-fixtures :once fix/init-rabbit-mq)
-
-(defn get-msg-from-rabbitmq [queue-name]
-  (with-open [ch (lch/open connection)]
-    (let [[meta payload] (lb/get ch queue-name false)]
-      (consumer/convert-and-ack-message ch meta payload))))
-
-(defn- get-msg-from-delay-queue []
-  (let [{:keys [queue-name queue-timeout-ms]} (:delay (rabbitmq-config))
-        queue-name (producer/delay-queue-name queue-name queue-timeout-ms)]
-    (get-msg-from-rabbitmq queue-name)))
-
-(defn- get-msg-from-dead-queue []
-  (let [{:keys [queue-name]} (:dead-letter (rabbitmq-config))]
-    (get-msg-from-rabbitmq queue-name)))
-
-(defn- get-msg-from-instant-queue []
-  (let [{:keys [queue-name]} (:instant (rabbitmq-config))]
-    (get-msg-from-rabbitmq queue-name)))
 
 (deftest mapper-func-test
   (let [message {:foo "bar"}]
@@ -49,7 +27,7 @@
         (with-redefs [metrics/message-unsuccessfully-processed! (fn []
                                                                   (reset! unsuccessfully-processed? true))]
           ((mapper-func (constantly :retry)) message)
-          (let [message-from-mq (get-msg-from-delay-queue)]
+          (let [message-from-mq (rmq/get-msg-from-delay-queue)]
             (is (= message-from-mq expected-message)))
           (is (= true @unsuccessfully-processed?)))))
 
@@ -62,25 +40,3 @@
             message)
           (is (= true @message-unsuccessfully-processed-fn-called?))
           (is (= true @sentry-report-fn-called?)))))))
-
-(deftest retry-test
-  (testing "message with a retry count of greater than 0 will publish to delay queue"
-    (let [message {:foo "bar" :retry-count 5}
-          expected-message {:foo "bar" :retry-count 4}]
-      (producer/retry message)
-      (let [message-from-mq (get-msg-from-delay-queue)]
-        (is (= expected-message message-from-mq)))))
-
-  (testing "message with a retry count of 0 will publish to dead queue"
-    (let [message {:foo "bar" :retry-count 0}
-          expected-message (dissoc message :retry-count)]
-      (producer/retry message)
-      (let [message-from-mq (get-msg-from-dead-queue)]
-        (is (= expected-message message-from-mq)))))
-
-  (testing "message with no retry count will publish to delay queue"
-    (let [message {:foo "bar"}
-          expected-message {:foo "bar" :retry-count 5}]
-      (producer/retry message)
-      (let [message-from-mq (get-msg-from-delay-queue)]
-        (is (= message-from-mq expected-message))))))
---
+++ test/ziggurat/messaging/producer_test.clj
@@ -0,0 +1,29 @@
+(ns ziggurat.messaging.producer_test
+  (:require [clojure.test :refer :all]
+            [ziggurat.fixtures :as fix]
+            [ziggurat.messaging.producer :as producer]
+            [ziggurat.util.rabbitmq :as rmq]))
+
+(use-fixtures :once fix/init-rabbit-mq)
+
+(deftest retry-test
+  (testing "message with a retry count of greater than 0 will publish to delay queue"
+    (let [message {:foo "bar" :retry-count 5}
+          expected-message {:foo "bar" :retry-count 4}]
+      (producer/retry message)
+      (let [message-from-mq (rmq/get-msg-from-delay-queue)]
+        (is (= expected-message message-from-mq)))))
+
+  (testing "message with a retry count of 0 will publish to dead queue"
+    (let [message {:foo "bar" :retry-count 0}
+          expected-message (dissoc message :retry-count)]
+      (producer/retry message)
+      (let [message-from-mq (rmq/get-msg-from-dead-queue)]
+        (is (= expected-message message-from-mq)))))
+
+  (testing "message with no retry count will publish to delay queue"
+    (let [message {:foo "bar"}
+          expected-message {:foo "bar" :retry-count 5}]
+      (producer/retry message)
+      (let [message-from-mq (rmq/get-msg-from-delay-queue)]
+        (is (= message-from-mq expected-message))))))
