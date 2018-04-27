7e2c8ca4beb8e1277ffe4c033b36511bd71332f5
Gurupratap <gurupratap@codeignition.co>
2018-04-27 18:04:23 +0530

    [Divya|Guru] Add tests for start func when actor routes is not specified

--- test/ziggurat/init_test.clj
+++ test/ziggurat/init_test.clj
@@ -26,12 +26,33 @@
         (is (= 5 (deref retry-count 10000 ::failure)))))))
 
 (deftest ziggurat-routes-serve-actor-routes
-  (testing "The routes added by actor should be served with ziggurat-routes"
+  (testing "The routes added by actor should be served along with ziggurat-routes"
     (with-redefs [streams/start-stream (constantly nil)
                   streams/stop-stream  (constantly nil)
                   config/config-file   "config.test.edn"]
       (init/start #() #() [["test-ping" (fn [_request] {:status 200
                                                         :body "pong"})]])
+      (let [{:keys [status body] :as response} (tu/get (-> (config/ziggurat-config) :http-server :port) "/test-ping" true false)
+            status-actor status
+            {:keys [status body] :as response} (tu/get (-> (config/ziggurat-config) :http-server :port) "/ping" true false)]
+        (init/stop #())
+        (is (= 200 status-actor))
+        (is (= 200 status)))))
+
+  (testing "The routes not added by actor should return 404"
+    (with-redefs [streams/start-stream (constantly nil)
+                  streams/stop-stream  (constantly nil)
+                  config/config-file   "config.test.edn"]
+      (init/start #() #() [])
       (let [{:keys [status body] :as response} (tu/get (-> (config/ziggurat-config) :http-server :port) "/test-ping" true false)]
-       (init/stop #())
-      (is (= 200 status))))))
+        (init/stop #())
+        (is (= 404 status)))))
+
+  (testing "The ziggurat routes should work fine when actor routes are not provided"
+    (with-redefs [streams/start-stream (constantly nil)
+                  streams/stop-stream  (constantly nil)
+                  config/config-file   "config.test.edn"]
+      (init/start #() #() [])
+      (let [{:keys [status body] :as response} (tu/get (-> (config/ziggurat-config) :http-server :port) "/ping" true false)]
+        (init/stop #())
+        (is (= 200 status))))))
