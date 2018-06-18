4a6aef7f79dbaa9eb5f43823898d02c07e8818d2
hogaur <hariom.gaur@go-jek.com>
2018-06-18 13:13:33 +0530

    GOJ-18209 | Pass topic-entity as keyword in producer specs

--- test/ziggurat/messaging/producer_test.clj
+++ test/ziggurat/messaging/producer_test.clj
@@ -14,28 +14,28 @@
 (deftest retry-test
   (testing "message with a retry count of greater than 0 will publish to delay queue"
     (fix/with-clear-data
-      (let [message {:foo "bar" :retry-count 5}
+      (let [message          {:foo "bar" :retry-count 5}
             expected-message {:foo "bar" :retry-count 4}
-            topic "booking"]
-        (producer/retry message topic)
+            topic-entity     :booking]
+        (producer/retry message topic-entity)
         (let [message-from-mq (rmq/get-msg-from-delay-queue "booking")]
           (is (= expected-message message-from-mq))))))
 
   (testing "message with a retry count of 0 will publish to dead queue"
     (fix/with-clear-data
-      (let [message {:foo "bar" :retry-count 0}
+      (let [message          {:foo "bar" :retry-count 0}
             expected-message (dissoc message :retry-count)
-            topic "booking"]
-        (producer/retry message topic)
+            topic-entity     :booking]
+        (producer/retry message topic-entity)
         (let [message-from-mq (rmq/get-msg-from-dead-queue "booking")]
           (is (= expected-message message-from-mq))))))
 
   (testing "message with no retry count will publish to delay queue"
     (fix/with-clear-data
-      (let [message {:foo "bar"}
-            expected-message {:foo "bar" :retry-count 5}
-            topic "booking"]
-        (producer/retry message topic)
+      (let [message           {:foo "bar"}
+            expected-message  {:foo "bar" :retry-count 5}
+            topic-entity      :booking]
+        (producer/retry message topic-entity)
         (let [message-from-mq (rmq/get-msg-from-delay-queue "booking")]
           (is (= message-from-mq expected-message)))))))
 
