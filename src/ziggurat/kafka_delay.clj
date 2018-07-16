43502dc5a49faf3ab93cca6c1e23141a62387c4c
Anirudh <anirudh2403@gmail.com>
2018-07-16 13:05:24 +0800

    Refacors code in kafka_delay namespace, renames variables

--- src/ziggurat/kafka_delay.clj
+++ src/ziggurat/kafka_delay.clj
@@ -8,10 +8,10 @@
 (defn get-current-time-in-millis []
   (.toEpochMilli (Instant/now)))
 
-(defn calculate-and-report-kafka-delay [metric-namespace recordTimestamp]
+(defn calculate-and-report-kafka-delay [metric-namespace record-timestamp]
   (let [now-millis (get-current-time-in-millis)
         delay      (- now-millis
-                      recordTimestamp)]
+                      record-timestamp)]
     (metrics/report-time metric-namespace delay)))
 
 
@@ -20,9 +20,9 @@
   (^void init [this ^ProcessorContext context]
     (do (set! processor-context context)
         nil))
-  (transform [this recordKey recordValue]
+  (transform [this record-key record-value]
     (do (calculate-and-report-kafka-delay metric-namespace (.timestamp processor-context))
-        (KeyValue/pair recordKey recordValue)))
+        (KeyValue/pair record-key record-value)))
   (punctuate [this ms] nil)
   (close [this] nil))
 
