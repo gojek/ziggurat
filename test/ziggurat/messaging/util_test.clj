4415e867c0b2117c890cd7414ae7813255dfb387
Rooba Limsa <rooba.limsa@go-jek.com>
2018-08-08 19:08:01 +0530

    GOJ-28017 | Hari/Rooba | change the queue name to be snake cased always

--- test/ziggurat/messaging/util_test.clj
+++ test/ziggurat/messaging/util_test.clj
@@ -3,8 +3,8 @@
             [ziggurat.messaging.util :refer [prefixed-queue-name]]))
 
 (deftest test-get-name-with-prefix-topic
-  (testing "given a topic-entity and queue-name, it returns a string with topic-entity as the prefix of queue-name"
-    (let [topic-entity    :topic
-          queue-name      "queue_name"
-          expected-string "topic_queue_name"]
+  (testing "given a topic-entity and queue-name, it returns a snake case string with topic-entity as the prefix of queue-name"
+    (let [topic-entity    :topic-name
+          queue-name      "queue-name"
+          expected-string "topic_name_queue_name"]
       (is (= (prefixed-queue-name topic-entity queue-name) expected-string)))))
