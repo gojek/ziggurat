af25920046540474049f5c8da5686ddf371aa036
Soham Kamani <sohamkamani@gmail.com>
2018-10-03 17:36:45 +0530

    Add tests for edge cases for thread pool couint

--- test/ziggurat/messaging/connection_test.clj
+++ test/ziggurat/messaging/connection_test.clj
@@ -8,12 +8,57 @@
 
 (use-fixtures :once fix/mount-only-config)
 
-(deftest retry-test
-  (testing "should provide the correct number of thread for the thread pool"
+(deftest start-connection-test
+  (testing "should provide the correct number of threads for the thread pool if channels are present"
     (let [thread-count (atom 0)
           orig-rmq-connect rmq/connect]
       (with-redefs [rmq/connect (fn [provided-config]
                                   (reset! thread-count (.getCorePoolSize (:executor provided-config)))
-                                  (orig-rmq-connect provided-config))]
+                                  (orig-rmq-connect provided-config))
+                    config/ziggurat-config (constantly {:jobs                 {:instant {:worker-count   4}}
+                                                        :retry                {:enabled [true :bool]}
+                                                        :stream-router        {:booking {:channels  {:channel-1 {:worker-count 10}}}}})]
         (mount/start (mount/only [#'connection]))
-        (is (= @thread-count 14))))))
+        (mount/stop)
+        (is (= @thread-count 14)))))
+
+  (testing "should provide the correct number of threads for the thread pool for multiple channels"
+    (let [thread-count (atom 0)
+          orig-rmq-connect rmq/connect]
+      (with-redefs [rmq/connect (fn [provided-config]
+                                  (reset! thread-count (.getCorePoolSize (:executor provided-config)))
+                                  (orig-rmq-connect provided-config))
+                    config/ziggurat-config (constantly {:jobs                 {:instant {:worker-count   4}}
+                                                        :retry                {:enabled [true :bool]}
+                                                        :stream-router        {:booking {:channels  {:channel-1 {:worker-count 5}
+                                                                                                     :channel-2 {:worker-count 10}}}}})]
+        (mount/start (mount/only [#'connection]))
+        (mount/stop)
+        (is (= @thread-count 19)))))
+
+  (testing "should provide the correct number of threads for the thread pool when channels are not present"
+    (let [thread-count (atom 0)
+          orig-rmq-connect rmq/connect]
+      (with-redefs [rmq/connect (fn [provided-config]
+                                  (reset! thread-count (.getCorePoolSize (:executor provided-config)))
+                                  (orig-rmq-connect provided-config))
+                    config/ziggurat-config (constantly {:jobs                 {:instant {:worker-count   4}}
+                                                        :retry                {:enabled true}
+                                                        :stream-router        {:booking {}}})]
+        (mount/start (mount/only [#'connection]))
+        (mount/stop)
+        (is (= @thread-count 4)))))
+
+  (testing "should provide the correct number of threads for the thread pool for multiple stream routes"
+    (let [thread-count (atom 0)
+          orig-rmq-connect rmq/connect]
+      (with-redefs [rmq/connect (fn [provided-config]
+                                  (reset! thread-count (.getCorePoolSize (:executor provided-config)))
+                                  (orig-rmq-connect provided-config))
+                    config/ziggurat-config (constantly {:jobs                 {:instant {:worker-count   4}}
+                                                        :retry                {:enabled [true :bool]}
+                                                        :stream-router        {:booking {:channels  {:channel-1 {:worker-count 10}}}
+                                                                               :driver {:channels  {:channel-1 {:worker-count 8}}}}})]
+        (mount/start (mount/only [#'connection]))
+        (mount/stop)
+        (is (= @thread-count 26))))))
