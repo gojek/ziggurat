c2a3a97d18bd87d9bd37ddd2825046949a580349
maulik.soneji <maulik.soneji@go-jek.com>
2018-11-14 15:39:33 +0530

    [Sravan/Maulik] Read auto offset value for stream from config

--- src/ziggurat/streams.clj
+++ src/ziggurat/streams.clj
@@ -20,13 +20,15 @@
            [java.util HashMap]))
 
 (defn- properties [{:keys [application-id bootstrap-servers stream-threads-count auto-offset-reset-config]}]
+  (if-not (contains? #{"latest" "earliest" nil} auto-offset-reset-config)
+            (throw (ex-info "Stream offset can only be latest or earliest" {:offset auto-offset-reset-config})))
   {StreamsConfig/APPLICATION_ID_CONFIG                    application-id
    StreamsConfig/BOOTSTRAP_SERVERS_CONFIG                 bootstrap-servers
    StreamsConfig/NUM_STREAM_THREADS_CONFIG                (int stream-threads-count)
    StreamsConfig/DEFAULT_KEY_SERDE_CLASS_CONFIG           (.getName (.getClass (Serdes/ByteArray)))
    StreamsConfig/DEFAULT_VALUE_SERDE_CLASS_CONFIG         (.getName (.getClass (Serdes/ByteArray)))
    StreamsConfig/DEFAULT_TIMESTAMP_EXTRACTOR_CLASS_CONFIG WallclockTimestampExtractor
-   ConsumerConfig/AUTO_OFFSET_RESET_CONFIG                auto-offset-reset-config})
+   ConsumerConfig/AUTO_OFFSET_RESET_CONFIG                (or auto-offset-reset-config "latest")})
 
 (defn- get-metric-namespace [default topic]
   (str (name topic) "." default))
