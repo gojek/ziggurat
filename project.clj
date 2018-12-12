1368d92c997096b0aa0e9f58f5f84a2e097e2a86
Rooba Limsa <rooba.limsa@go-jek.com>
2018-12-12 13:30:00 +0530

    Update version and changelog for  2.7.0

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
 (cemerick.pomegranate.aether/register-wagon-factory!
   "http" #(org.apache.maven.wagon.providers.http.HttpWagon.))
 
-(defproject com.gojek.lambda/ziggurat "2.6.3"
+(defproject com.gojek.lambda/ziggurat "2.7.0"
   :description "The actor framework for Project Lambda"
   :url "https://github.com/gojektech/ziggurat"
   :license {:name "Apache License, Version 2.0"
