2f9b26103ff1bb0079f5bd687b2ae1bc0d88ec3e
Saptanto Sindu <saptanto.sindu@gmail.com>
2019-02-15 18:35:42 +0530

    Fixing Deadset API on channel enabled

--- src/ziggurat/resource/dead_set.clj
+++ src/ziggurat/resource/dead_set.clj
@@ -4,6 +4,9 @@
             [ziggurat.config :refer [get-in-config]]
             [ziggurat.messaging.dead-set :as r]
             [mount.core :as mount]))
+(def not-found-for-retry
+  {:status 404
+   :body {:error "Retry is not enabled"}})
 
 (defn- validate-count [count]
   (let [schema (s/constrained s/Num #(<= 0 % Integer/MAX_VALUE))]
@@ -23,48 +26,55 @@
   (and (some? (get-in stream-routes [(keyword topic-entity) (or (keyword channel) :handler-fn)]))
        (validate-count count)))
 
-(defn- not-found-handler-for-retry [_]
-  {:status 404
-   :body {:error "Retry is not enabled"}})
+(defn- channel-request? [channel]
+  (some? channel))
 
 (defn retry-enabled? []
   (get-in-config [:retry :enabled]))
 
+(defn channel-retry-enabled? [topic-entity stream-routes channel]
+  (get-in stream-routes [(keyword topic-entity) (keyword channel) :retry :enabled]))
+
+(defn retry-allowed? [topic-entity stream-routes channel]
+  (if (channel-request? channel)
+    (channel-retry-enabled? topic-entity stream-routes channel)
+    (retry-enabled?)))
+
 (defn get-replay []
-  (if-not (retry-enabled?)
-    not-found-handler-for-retry
-    (let [stream-routes (:stream-routes (mount/args))]
-      (fn [{{:keys [count topic-entity channel]} :params}]
-        (let [parsed-count (parse-count count)]
+  (let [stream-routes (:stream-routes (mount/args))]
+    (fn [{{:keys [count topic-entity channel]} :params}]
+      (let [parsed-count (parse-count count)]
+        (if (retry-allowed? topic-entity stream-routes channel)
           (if (validate-params parsed-count topic-entity stream-routes channel)
             (do (r/replay parsed-count topic-entity channel)
                 {:status 200
                  :body   {:message "Requeued messages on the queue for retrying"}})
             {:status 400
-             :body   {:error "Count should be the positive integer and topic entity/ channel should be present"}}))))))
+             :body   {:error "Count should be the positive integer and topic entity/ channel should be present"}})
+          not-found-for-retry)))))
 
 (defn get-view []
-  (if-not (retry-enabled?)
-    not-found-handler-for-retry
-    (let [stream-routes (:stream-routes (mount/args))]
-      (fn view [{{:keys [count topic-entity channel]} :params}]
-        (let [parsed-count (parse-count count)]
+  (let [stream-routes (:stream-routes (mount/args))]
+    (fn view [{{:keys [count topic-entity channel]} :params}]
+      (let [parsed-count (parse-count count)]
+        (if (retry-allowed? topic-entity stream-routes channel)
           (if (validate-params parsed-count topic-entity stream-routes channel)
             {:status 200
              :body   {:messages (r/view parsed-count topic-entity channel)}}
             {:status 400
-             :body   {:error "Count should be the positive integer and topic entity/ channel should be present"}}))))))
+             :body   {:error "Count should be the positive integer and topic entity/ channel should be present"}})
+          not-found-for-retry)))))
 
 (defn delete-messages []
-  (if-not (retry-enabled?)
-    not-found-handler-for-retry
-    (let [stream-routes (:stream-routes (mount/args))]
-      (fn [{{:keys [count topic-entity channel]} :params}]
-        (let [parsed-count (parse-count count)]
+  (let [stream-routes (:stream-routes (mount/args))]
+    (fn [{{:keys [count topic-entity channel]} :params}]
+      (let [parsed-count (parse-count count)]
+        (if (retry-allowed? topic-entity stream-routes channel)
           (if (validate-params parsed-count topic-entity stream-routes channel)
             (do
               (r/delete parsed-count topic-entity channel)
               {:status 200
                :body   {:message "Deleted messages successfully"}})
             {:status 400
-             :body   {:error "Count should be the positive integer and topic entity/ channel should be present"}}))))))
+             :body   {:error "Count should be the positive integer and topic entity/ channel should be present"}})
+          not-found-for-retry)))))
--- test/ziggurat/server/routes_test.clj
+++ test/ziggurat/server/routes_test.clj
@@ -3,13 +3,43 @@
             [mount.core :as mount]
             [ziggurat.config :refer [ziggurat-config]]
             [ziggurat.messaging.dead-set :as ds]
-            [ziggurat.resource.dead-set :refer [retry-enabled?]]
+            [ziggurat.resource.dead-set :refer [retry-enabled? channel-retry-enabled?]]
             [ziggurat.fixtures :as fix]
             [ziggurat.server.test-utils :as tu]
             [ziggurat.server :refer [server]]
             [cheshire.core :as json]))
 
 (use-fixtures :each fix/silence-logging)
+
+(deftest router-dead-set-channel-disabled-test
+  (let [stream-routes {:default {:handler-fn (fn [])
+                                 :channel-1 (fn [])}}]
+    (with-redefs [retry-enabled? (fn [] false)]
+      (fix/with-start-server
+        stream-routes
+        (testing "should return 404 when /v1/dead_set/replay for channel is called and channel retry is disabled"
+          (with-redefs [channel-retry-enabled? (constantly false)]
+            (let [params {:count "10" :topic-entity "default" :channel "channel-1"}
+                  {:keys [status body]} (tu/post (-> (ziggurat-config) :http-server :port) "/v1/dead_set/replay" params)
+                  expected-body {:error "Retry is not enabled"}]
+              (is (= 404 status))
+              (is (= expected-body (json/decode body true))))))
+
+        (testing "should return 404 when /v1/dead_set for channel is called and channel retry is disabled"
+          (with-redefs [channel-retry-enabled? (constantly false)]
+            (let [params {:count "10" :topic-entity "default" :channel "channel-1"}
+                  {:keys [status body]} (tu/get (-> (ziggurat-config) :http-server :port) "/v1/dead_set" true true {} params)
+                  expected-body {:error "Retry is not enabled"}]
+              (is (= 404 status))
+              (is (= expected-body body)))))
+
+        (testing "should return 404 when delete /v1/dead_set for channel is called and channel retry is disabled"
+          (with-redefs [channel-retry-enabled? (constantly false)]
+            (let [params {:count "10" :topic-entity "default" :channel "channel-1"}
+                  {:keys [status body]} (tu/delete (-> (ziggurat-config) :http-server :port) "/v1/dead_set" true true {} params)
+                  expected-body {:error "Retry is not enabled"}]
+              (is (= 404 status))
+              (is (= expected-body body)))))))))
 
 (deftest router-dead-set-disabled-test
   (let [stream-routes {:default {:handler-fn (fn [])}}]
@@ -37,8 +67,8 @@
             (is (= expected-body (json/decode body true)))))))))
 
 (deftest router-dead-set-enabled-test
-  (with-redefs [retry-enabled? (constantly true)]
-    (let [stream-routes {:default {:handler-fn (fn [])}}]
+  (let [stream-routes {:default {:handler-fn (fn [])}}]
+    (with-redefs [retry-enabled? (fn [] true)]
       (fix/with-start-server
         stream-routes
         (testing "Should return 200 ok when GET /ping is called"
@@ -109,18 +139,19 @@
               (is (= expected-body body)))))
 
         (testing "should return 400 when get /v1/dead_set is called with invalid channel"
-          (with-redefs [ds/view (fn [_ _ _] nil)]
-            (let [expected-body {:error "Count should be the positive integer and topic entity/ channel should be present"}
-                  count "10"
-                  params {:count count :topic-entity "default" :channel "invalid"}
-                  {:keys [status body]} (tu/get (-> (ziggurat-config) :http-server :port)
-                                                "/v1/dead_set"
-                                                true
-                                                true
-                                                {}
-                                                params)]
-              (is (= 400 status))
-              (is (= expected-body body)))))
+          (with-redefs [channel-retry-enabled? (constantly true)]
+            (with-redefs [ds/view (fn [_ _ _] nil)]
+              (let [expected-body {:error "Count should be the positive integer and topic entity/ channel should be present"}
+                    count "10"
+                    params {:count count :topic-entity "default" :channel "invalid"}
+                    {:keys [status body]} (tu/get (-> (ziggurat-config) :http-server :port)
+                                                  "/v1/dead_set"
+                                                  true
+                                                  true
+                                                  {}
+                                                  params)]
+                (is (= 400 status))
+                (is (= expected-body body))))))
 
         (testing "should return 200 when get /v1/dead_set is called with valid count val"
           (with-redefs [ds/view (fn [_ _ _] {:foo "bar"})]
@@ -189,15 +220,16 @@
               (is (= expected-body body)))))
 
         (testing "should return 400 when delete /v1/dead_set is called with invalid channel"
-          (with-redefs [ds/view (fn [_ _ _] nil)]
-            (let [expected-body {:error "Count should be the positive integer and topic entity/ channel should be present"}
-                  count "10"
-                  params {:count count :topic-entity "default" :channel "invalid"}
-                  {:keys [status body]} (tu/delete (-> (ziggurat-config) :http-server :port)
-                                                   "/v1/dead_set"
-                                                   true
-                                                   true
-                                                   {}
-                                                   params)]
-              (is (= 400 status))
-              (is (= expected-body body)))))))))
+          (with-redefs [channel-retry-enabled? (constantly true)]
+            (with-redefs [ds/view (fn [_ _ _] nil)]
+              (let [expected-body {:error "Count should be the positive integer and topic entity/ channel should be present"}
+                    count "10"
+                    params {:count count :topic-entity "default" :channel "invalid"}
+                    {:keys [status body]} (tu/delete (-> (ziggurat-config) :http-server :port)
+                                                     "/v1/dead_set"
+                                                     true
+                                                     true
+                                                     {}
+                                                     params)]
+                (is (= 400 status))
+                (is (= expected-body body))))))))))
