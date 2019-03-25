5738774a9f117e48646277bcd48dedb8be03ef65
Anirudh <anirudh2403@gmail.com>
2019-03-25 17:59:29 +0530

    Adds a make-file and changes container name in docker-compose

---
+++ Makefile
@@ -0,0 +1,16 @@
+.PHONY: all
+all: test-all test
+
+setup:
+	lein deps
+	docker-compose down && docker-compose up -d
+	sleep 10
+	docker exec -it ziggurat_kafka /opt/bitnami/kafka/bin/kafka-topics.sh --create --topic topic --partitions 3 --replication-factor 1 --zookeeper ziggurat_zookeeper
+
+test-all: setup
+	lein test-all
+	docker-compose down
+
+test: setup
+	lein test
+	docker-compose down
--- bin/run_tests_in_ci.sh
+++ bin/run_tests_in_ci.sh
@@ -7,6 +7,6 @@ lein deps
 mv -fv resources/config.test.{ci.edn,edn}
 docker-compose up -d
 sleep 15
-docker exec -it ziggurat_kafka1_1 /opt/bitnami/kafka/bin/kafka-topics.sh --create --topic topic --partitions 3 --replication-factor 1 --zookeeper ziggurat_zookeeper_1
+docker exec -it ziggurat_kafka /opt/bitnami/kafka/bin/kafka-topics.sh --create --topic topic --partitions 3 --replication-factor 1 --zookeeper ziggurat_zookeeper
 lein test-all
 docker-compose down
