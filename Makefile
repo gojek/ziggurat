79baf58bea9459cc96328e461e35095271622fdd
Anirudh <anirudh2403@gmail.com>
2019-03-26 12:34:05 +0530

    Runs make test-all command in ci

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ all: test-all test
 
 setup:
 	lein deps
-	docker-compose down && docker-compose up -d
+	docker-compose up -d
 	sleep 10
 	docker exec -it ziggurat_kafka /opt/bitnami/kafka/bin/kafka-topics.sh --create --topic topic --partitions 3 --replication-factor 1 --zookeeper ziggurat_zookeeper
 
--- bin/run_tests_in_ci.sh
+++ bin/run_tests_in_ci.sh
@@ -3,10 +3,5 @@
 set -ex
 
 lein clean
-lein deps
 mv -fv resources/config.test.{ci.edn,edn}
-docker-compose up -d
-sleep 15
-docker exec -it ziggurat_kafka /opt/bitnami/kafka/bin/kafka-topics.sh --create --topic topic --partitions 3 --replication-factor 1 --zookeeper ziggurat_zookeeper
-lein test-all
-docker-compose down
+make test-all
