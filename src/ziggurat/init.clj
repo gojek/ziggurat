3f23acb2fcf2959717189f9c66c2d6a4ba717a66
Kartik Gupta <rulz.gupta@gmail.com>
2019-05-13 16:57:28 +0530

    Extracts out states per mode

--- src/ziggurat/init.clj
+++ src/ziggurat/init.clj
@@ -3,7 +3,6 @@
   (:require [clojure.tools.logging :as log]
             [mount.core :as mount :refer [defstate]]
             [schema.core :as s]
-            [sentry-clj.async :as sentry]
             [ziggurat.config :refer [ziggurat-config] :as config]
             [ziggurat.metrics :as metrics]
             [ziggurat.messaging.connection :as messaging-connection]
@@ -28,47 +27,91 @@
        (mount/with-args args)
        (mount/start))))
 
-(defn start
-  "Starts up Ziggurat's config, reporters, actor fn, rabbitmq connection and then streams, server etc"
-  [actor-start-fn stream-routes actor-routes]
+(defn start-rabbitmq-connection [stream-routes]
+  (start* #{#'messaging-connection/connection} {:stream-routes stream-routes}))
+
+(defn stop-rabbitmq-connection []
+  (mount/stop #'messaging-connection/connection))
+
+(defn start-rabbitmq-consumers [stream-routes]
+  (start-rabbitmq-connection stream-routes)
+  (messaging-consumer/start-subscribers stream-routes))
+
+(defn start-rabbitmq-producers [stream-routes]
+  (start-rabbitmq-connection stream-routes)
+  (messaging-producer/make-queues stream-routes))
+
+(defn start-stream [stream-routes]
+  (start-rabbitmq-producers stream-routes)
+  (start* #{#'streams/stream} stream-routes))
+
+(defn start-management-apis [stream-routes]
+  (start-rabbitmq-connection stream-routes)
+  (start* #{#'server/server} {:stream-routes stream-routes}))
+
+(defn start-server [stream-routes actor-routes]
+  (start-rabbitmq-connection stream-routes)
+  (start* #{#'server/server} {:stream-routes stream-routes
+                              :actor-routes  actor-routes}))
+
+(defn start-common-states []
   (start* #{#'config/config
             #'statsd-reporter
-            #'sentry-reporter})
-  (actor-start-fn)
-  (start* #{#'messaging-connection/connection} {:stream-routes stream-routes})
-  (messaging-producer/make-queues stream-routes)
-  (messaging-consumer/start-subscribers stream-routes)      ;; We want subscribers to start after creating queues on RabbitMQ.
-  (start* #{#'server/server
-            #'nrepl-server/server
-            #'streams/stream}
-          {:stream-routes stream-routes
-           :actor-routes  actor-routes}))
+            #'sentry-reporter
+            #'nrepl-server/server}))
 
-(defn stop
-  "Calls the Ziggurat's state stop fns and then actor-stop-fn."
-  [actor-stop-fn]
+(defn stop-common-states []
   (mount/stop #'config/config
               #'statsd-reporter
               #'messaging-connection/connection
               #'server/server
               #'nrepl-server/server
-              #'streams/stream)
+              #'streams/stream))
+
+(defn stop-server []
+  (mount/stop #'server/server)
+  (stop-rabbitmq-connection))
+
+(defn stop-stream []
+  (mount/stop #'streams/stream)
+  (stop-rabbitmq-connection))
+
+(defn stop-management-apis []
+  (mount/stop #'server/server)
+  (stop-rabbitmq-connection))
+
+(defn start
+  "Starts up Ziggurat's config, reporters, actor fn, rabbitmq connection and then streams, server etc"
+  [actor-start-fn stream-routes actor-routes]
+  (start-common-states)
+  (actor-start-fn)
+  (start-stream stream-routes)
+  (start-server stream-routes actor-routes)
+  (start-rabbitmq-consumers stream-routes))     ;; We want subscribers to start after creating queues on RabbitMQ.
+
+
+(defn stop
+  "Calls the Ziggurat's state stop fns and then actor-stop-fn."
+  [actor-stop-fn]
+  (stop-common-states)
+  (stop-stream)
+  (stop-server)
   (actor-stop-fn)
   (mount/stop #'config/config))
 
 (defn- add-shutdown-hook [actor-stop-fn]
   (.addShutdownHook
-   (Runtime/getRuntime)
-   (Thread. ^Runnable #(do (stop actor-stop-fn)
-                           (shutdown-agents))
-            "Shutdown-handler")))
+    (Runtime/getRuntime)
+    (Thread. ^Runnable #(do (stop actor-stop-fn)
+                            (shutdown-agents))
+             "Shutdown-handler")))
 
 (s/defschema StreamRoute
   (s/conditional
-   #(and (seq %)
-         (map? %))
-   {s/Keyword {:handler-fn (s/pred #(fn? %))
-               s/Keyword   (s/pred #(fn? %))}}))
+    #(and (seq %)
+          (map? %))
+    {s/Keyword {:handler-fn (s/pred #(fn? %))
+                s/Keyword   (s/pred #(fn? %))}}))
 
 (defn validate-stream-routes [stream-routes]
   (s/validate StreamRoute stream-routes))
