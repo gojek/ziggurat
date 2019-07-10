661652eb5cf605b53a39805e68a1231ddcd8fe2c
Anirudh <anirudh2403@gmail.com>
2019-07-10 17:01:38 +0530

    Refactors default middleware proto->hash

--- src/ziggurat/middleware/default.clj
+++ src/ziggurat/middleware/default.clj
@@ -5,21 +5,22 @@
             [ziggurat.sentry :refer [sentry-reporter]]
             [ziggurat.config :refer [ziggurat-config]]))
 
-(defn- deserialise-message [message proto-class]
+(defn- deserialise-message
+  "This function takes in the message(proto Byte Array) and the proto-class and deserializes the proto ByteArray into a
+  Clojure PersistentHashMap.
+  Temporary logic for migration of services to Ziggurat V3.0
+    If the message is of type map, the function just returns the map as it is. In older versions of Ziggurat (< 3.0) we stored
+    the messages in deserialized formats in RabbitMQ and those messages can be processed by this function. So we have this logic here."
+  [message proto-class topic-entity-name]
   (if-not (map? message)
-    (let [proto-klass  (proto/protodef proto-class)
-          loaded-proto (proto/protobuf-load proto-klass message)
-          proto-keys   (-> proto-klass
-                           proto/protobuf-schema
-                           :fields
-                           keys)]
-      (select-keys loaded-proto proto-keys))
-    message))
-
-(defn protobuf->hash [handler-fn proto-class topic-entity-name]
-  (fn [message]
     (try
-      (handler-fn (deserialise-message message proto-class))
+      (let [proto-klass  (proto/protodef proto-class)
+            loaded-proto (proto/protobuf-load proto-klass message)
+            proto-keys   (-> proto-klass
+                             proto/protobuf-schema
+                             :fields
+                             keys)]
+        (select-keys loaded-proto proto-keys))
       (catch Throwable e
         (let [service-name      (:app-name (ziggurat-config))
               additional-tags   {:topic_name topic-entity-name}
@@ -28,4 +29,11 @@
               multi-namespaces  [metric-namespaces [default-namespace]]]
           (sentry/report-error sentry-reporter e (str "Couldn't parse the message with proto - " proto-class))
           (metrics/multi-ns-increment-count multi-namespaces "failed" additional-tags)
-          (handler-fn nil))))))
+          nil)))
+    message))
+
+(defn protobuf->hash
+  "This is a middleware function that takes in a message (Proto ByteArray or PersistentHashMap) and calls the handler-fn with the deserialized PersistentHashMap"
+  [handler-fn proto-class topic-entity-name]
+  (fn [message]
+    (handler-fn (deserialise-message message proto-class topic-entity-name))))
