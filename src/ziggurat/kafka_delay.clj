c8f8e372f068758bd93fa1295d56386db5838ff5
Anirudh <anirudh2403@gmail.com>
2019-12-02 14:44:54 +0530

    report-histogram is now reported across both influx and cortex

--- src/ziggurat/kafka_delay.clj
+++ src/ziggurat/kafka_delay.clj
@@ -3,9 +3,11 @@
             [ziggurat.util.time :refer :all]))
 
 (defn calculate-and-report-kafka-delay
-  ([metric-namespace record-timestamp]
-   (calculate-and-report-kafka-delay metric-namespace record-timestamp nil))
-  ([metric-namespace record-timestamp additional-tags]
-   (let [now-millis (get-current-time-in-millis)
-         delay      (- now-millis record-timestamp)]
-     (metrics/report-histogram metric-namespace delay additional-tags))))
+  ([metric-namespaces record-timestamp]
+   (calculate-and-report-kafka-delay metric-namespaces record-timestamp nil))
+  ([metric-namespaces record-timestamp additional-tags]
+   (let [now-millis        (get-current-time-in-millis)
+         delay             (- now-millis record-timestamp)
+         default-namespace (last metric-namespaces)
+         multi-namespaces  [metric-namespaces [default-namespace]]]
+     (metrics/multi-ns-report-histogram multi-namespaces delay additional-tags))))
