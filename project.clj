9565cf94403058ac1bc4c7091c18958aad2f2e8f
prateek.khatri <prateek.khatri@go-jek.com>
2020-05-20 11:28:29 +0530

    changing project version to 3.3.0-alpha.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tech.gojek/ziggurat "3.3.0-alpha.7"
+(defproject tech.gojek/ziggurat "3.3.0-alpha.8"
   :description "A stream processing framework to build stateless applications on kafka"
   :url "https://github.com/gojektech/ziggurat"
   :license {:name "Apache License, Version 2.0"
