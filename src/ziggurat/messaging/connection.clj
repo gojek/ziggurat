ba55eaa244afb7c1b67826851c23f26e4c932bc0
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-06-11 20:27:14 +0530

    remove imports and require statements

--- src/ziggurat/messaging/connection.clj
+++ src/ziggurat/messaging/connection.clj
@@ -1,11 +1,2 @@
-(ns ziggurat.messaging.connection
-  (:require [clojure.tools.logging :as log]
-            [langohr.core :as rmq]
-            [mount.core :as mount :refer [defstate start]]
-            [sentry-clj.async :as sentry]
-            [ziggurat.config :refer [ziggurat-config]]
-            [ziggurat.sentry :refer [sentry-reporter]]
-            [ziggurat.channel :refer [get-keys-for-topic]])
-  (:import [com.rabbitmq.client ShutdownListener]
-           [java.util.concurrent Executors]))
+
 
