b2be20c15b06e4d906c19dc0b33cd598ccfc042c
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-06-25 14:18:48 +0530

    adds missing function and get-implementation function to perform a nil check

--- src/ziggurat/messaging/messaging.clj
+++ src/ziggurat/messaging/messaging.clj
@@ -8,6 +8,11 @@
 
 (def messaging-impl (atom nil))
 
+(defn- get-implementation []
+  (if (nil? @messaging-impl)
+    (throw (Exception. "Messaging Library is not initialized"))
+    messaging-impl))
+
 (defn- get-messaging-implementor-constructor []
   (if-let [configured-metrics-class-constructor (get-in (ziggurat-config) [:messaging :constructor])]
     (let [configured-constructor-symbol (symbol configured-metrics-class-constructor)
@@ -27,30 +32,36 @@
 (defn start-connection [config stream-routes]
   (do (initialise-messaging-library)
       (log/info "Initializing Messaging Library")
-      (messaging-interface/start-connection @messaging-impl config stream-routes)))
+      (messaging-interface/start-connection (get-implementation) config stream-routes)))
 
 (defn stop-connection [config stream-routes]
   (do (log/info "Stopping Messaging Library")
-      (messaging-interface/stop-connection @messaging-impl config stream-routes)))
+      (messaging-interface/stop-connection (get-implementation) config stream-routes)))
+
+(defn create-and-bind-queue
+  ([queue-name exchange-name]
+   (create-and-bind-queue queue-name exchange-name nil))
+  ([queue-name exchange-name dead-letter-exchange]
+   (messaging-interface/create-and-bind-queue (get-implementation) queue-name exchange-name dead-letter-exchange)))
 
 (defn publish
   ([exchange message-payload]
-   (messaging-interface/publish @messaging-impl exchange message-payload))
+   (messaging-interface/publish (get-implementation) exchange message-payload))
   ([exchange message-payload expiration]
-   (messaging-interface/publish @messaging-impl exchange message-payload expiration)))
+   (messaging-interface/publish (get-implementation) exchange message-payload expiration)))
 
 (defn get-messages-from-queue
   ([queue-name ack?]
-   (messaging-interface/get-messages-from-queue @messaging-impl queue-name ack?))
+   (messaging-interface/get-messages-from-queue (get-implementation) queue-name ack?))
   ([queue-name ack? count]
-   (messaging-interface/get-messages-from-queue @messaging-impl queue-name ack? count)))
+   (messaging-interface/get-messages-from-queue (get-implementation) queue-name ack? count)))
 
 (defn process-messages-from-queue [queue-name count processing-fn]
-  (messaging-interface/process-messages-from-queue @messaging-impl queue-name count processing-fn))
+  (messaging-interface/process-messages-from-queue (get-implementation) queue-name count processing-fn))
 
 (defn start-subscriber [prefetch-count wrapped-mapper-fn queue-name]
-  (messaging-interface/start-subscriber @messaging-impl prefetch-count wrapped-mapper-fn queue-name))
+  (messaging-interface/start-subscriber (get-implementation) prefetch-count wrapped-mapper-fn queue-name))
 
 (defn consume-message [ch meta payload ack?]
-  (messaging-interface/consume-message @messaging-impl ch meta payload ack?))
+  (messaging-interface/consume-message (get-implementation) ch meta payload ack?))
 
