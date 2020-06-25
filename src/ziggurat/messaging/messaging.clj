3d5d87ba222bd4048ce545716de1d8b7c9a0c203
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-06-25 09:03:34 +0530

    "adds interface methods"

--- src/ziggurat/messaging/messaging.clj
+++ src/ziggurat/messaging/messaging.clj
@@ -35,3 +35,24 @@
   (do (log/info "Stopping Messaging Library")
       (messaging-interface/stop-connection @messaging-impl config stream-routes)))
 
+(defn publish
+  ([exchange message-payload]
+   (messaging-interface/publish @messaging-impl exchange message-payload))
+  ([exchange message-payload expiration]
+   (messaging-interface/publish @messaging-impl exchange message-payload expiration)))
+
+(defn get-messages-from-queue
+  ([queue-name ack?]
+   (messaging-interface/get-messages-from-queue @messaging-impl queue-name ack?))
+  ([queue-name ack? count]
+   (messaging-interface/get-messages-from-queue @messaging-impl queue-name ack? count)))
+
+(defn process-messages-from-queue [queue-name count processing-fn]
+  (messaging-interface/process-messages-from-queue @messaging-impl queue-name count processing-fn))
+
+(defn start-subscriber [prefetch-count wrapped-mapper-fn queue-name]
+  (messaging-interface/start-subscriber @messaging-impl prefetch-count wrapped-mapper-fn queue-name))
+
+(defn consume-message [ch meta payload ack?]
+  (messaging-interface/consume-message @messaging-impl ch meta payload ack?))
+
