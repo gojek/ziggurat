d5666785a75a5a8e64fe1f82219d1a5da5d03b8c
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-06-27 10:03:10 +0530

    adds missing functions and methods to mock messaging ns

--- test/ziggurat/util/mock_messaging_implementation.clj
+++ test/ziggurat/util/mock_messaging_implementation.clj
@@ -3,24 +3,35 @@
             [ziggurat.messaging.messaging-interface :refer [MessagingProtocol]]))
 
 (defn start-connection [config stream-routes] nil)
+
 (defn stop-connection [config stream-routes] nil)
 
 (defn publish
   ([exchange message-payload] nil)
   ([exchange message-payload expiration] nil))
 
+(defn create-and-bind-queue
+  ([queue-name exchange-name] nil)
+  ([queue-name exchange-name dead-letter-exchange] nil))
+
 (defn get-messages-from-queue
   ([queue-name ack?] nil)
   ([queue-name ack? count] nil))
 
 (defn process-messages-from-queue [queue-name count processing-fn] nil)
+
 (defn start-subscriber [prefetch-count wrapped-mapper-fn queue-name] nil)
+
 (defn consume-message [ch meta payload ack?] nil)
 
 (deftype MockMessaging []
   MessagingProtocol
   (start-connection [impl config stream-routes] (start-connection config stream-routes))
   (stop-connection [impl config stream-routes] (stop-connection config stream-routes))
+  (create-and-bind-queue [impl queue-name exchange-name]
+    (create-and-bind-queue queue-name exchange-name))
+  (create-and-bind-queue [impl queue-name exchange-name dead-letter-exchange]
+    (create-and-bind-queue queue-name exchange-name dead-letter-exchange))
   (publish [impl exchange message-payload] (publish exchange message-payload))
   (publish [impl exchange message-payload expiration] (publish exchange message-payload expiration))
   (get-messages-from-queue [impl queue-name ack?] (get-messages-from-queue queue-name ack?))
