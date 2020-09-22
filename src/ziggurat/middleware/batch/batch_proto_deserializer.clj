db4d967d68c313d87e23f604b445599aee77d6eb
prateek.khatri <prateek.khatri@go-jek.com>
2020-09-22 20:40:45 +0530

    Changing the logic in batch proto deserializer to deserialize both key and value in the batch message

--- src/ziggurat/middleware/batch/batch_proto_deserializer.clj
+++ src/ziggurat/middleware/batch/batch_proto_deserializer.clj
@@ -1,9 +1,20 @@
 (ns ziggurat.middleware.batch.batch-proto-deserializer
   (:require [ziggurat.middleware.default :refer [deserialize-message]]))
 
+(defn- deserialize-key-and-value
+  [key-proto-class value-proto-class topic-entity]
+  (fn [message]
+    (let [key                (:key message)
+          value              (:value message)
+          deserialized-key   (when (some? key) (deserialize-message key key-proto-class (name topic-entity)))
+          deserialized-value (when (some? value) (deserialize-message value value-proto-class (name topic-entity)))]
+      (-> (assoc message :key deserialized-key)
+          (assoc :value deserialized-value)))))
+
 (defn deserialize-batch-of-proto-messages
   "This is a middleware function that takes in a sequence of proto message and calls forms a lazy sequence of
    de-serialized messages before passing it to the handler-fn"
-  [handler-fn proto-class topic-entity-name]
+  [handler-fn key-proto-class value-proto-class topic-entity]
   (fn [batch-message]
-    (handler-fn (map #(deserialize-message % proto-class topic-entity-name) batch-message))))
+    (let [key-value-deserializer (deserialize-key-and-value key-proto-class value-proto-class topic-entity)]
+      (handler-fn (map #(key-value-deserializer %) batch-message)))))
--- test/ziggurat/middleware/batch/batch_proto_deserializer_test.clj
+++ test/ziggurat/middleware/batch/batch_proto_deserializer_test.clj
@@ -1,22 +1,53 @@
 (ns ziggurat.middleware.batch.batch-proto-deserializer-test
   (:require [clojure.test :refer :all])
   (:require [ziggurat.middleware.batch.batch-proto-deserializer :refer :all]
-            [protobuf.core :as proto]
-            [ziggurat.middleware.batch.batch-proto-deserializer :as mw])
-  (:import (flatland.protobuf.test Example$Photo)))
+            [protobuf.core :as proto])
+  (:import (flatland.protobuf.test Example$Photo Example$Photo$Tag)))
 
 (deftest batch-proto-deserializer-test
-  (let [message            {:id 7 :path "/photos/h2k3j4h9h23"}
-        proto-class        Example$Photo
-        topic-entity-name  "test"
-        batch-message      (repeat 10 (proto/->bytes (proto/create Example$Photo message)))
-        handler-fn-called? (atom false)
-        handler-fn         (fn [batch]
-                             (is (seq? batch))
-                             (loop [msg batch]
-                               (when (not-empty msg)
-                                (is (= message (first msg)))
-                                (recur (rest msg))))
-                             (reset! handler-fn-called? true))]
-    ((mw/deserialize-batch-of-proto-messages handler-fn proto-class topic-entity-name) batch-message)
-    (is (true? @handler-fn-called?))))
+  (testing "deserializes key and value for all the messages in a batch"
+    (let [key                    {:person-id 100}
+          value                  {:id 7 :path "/photos/h2k3j4h9h23"}
+          key-proto-class        Example$Photo$Tag
+          value-proto-class      Example$Photo
+          serialized-key         (proto/->bytes (proto/create key-proto-class key))
+          serialized-value       (proto/->bytes (proto/create value-proto-class value))
+          batch-message          (repeat 5 {:key serialized-key :value serialized-value})
+          handler-fn-called?     (atom false)
+          handler-fn             (fn [batch]
+                                   (is (seq? batch))
+                                   (loop [msg batch]
+                                     (when (not-empty msg)
+                                       (let [key-value-pair (first msg)]
+                                         (is (map? key-value-pair))
+                                         (is (= key (:key key-value-pair)))
+                                         (is (= value (:value key-value-pair)))
+                                         (recur (rest msg)))))
+                                   (reset! handler-fn-called? true))]
+      ((deserialize-batch-of-proto-messages handler-fn key-proto-class value-proto-class :some-topic-entity) batch-message)
+      (is (true? @handler-fn-called?)))))
+
+(deftest batch-proto-deserializer-test-with-nil-values
+  (testing "Does not deserialize nil key or value"
+    (let [key                    {:person-id 100}
+          value                  {:id 7 :path "/photos/h2k3j4h9h23"}
+          key-proto-class        Example$Photo$Tag
+          value-proto-class      Example$Photo
+          serialized-key         (proto/->bytes (proto/create key-proto-class key))
+          serialized-value       (proto/->bytes (proto/create value-proto-class value))
+          batch-message          [{:key nil :value serialized-value} {:key serialized-key :value nil}]
+          handler-fn-called?     (atom false)
+          handler-fn             (fn [batch]
+                                   (is (seq? batch))
+                                   (loop [msg batch]
+                                     (when (not-empty msg)
+                                       (let [key-value-pair (first msg)
+                                             deserialized-key (:key key-value-pair)
+                                             deserialized-value (:value key-value-pair)]
+                                         (is (map? key-value-pair))
+                                         (is (or (nil? deserialized-key) (= key deserialized-key)))
+                                         (is (or (nil? deserialized-value) (= value deserialized-value)))
+                                         (recur (rest msg)))))
+                                   (reset! handler-fn-called? true))]
+      ((deserialize-batch-of-proto-messages handler-fn key-proto-class value-proto-class :some-topic-entity) batch-message)
+      (is (true? @handler-fn-called?)))))
