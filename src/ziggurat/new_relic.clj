ef02b0f7967278001d42ebf684805020003aee16
Anmol Vijaywargiya <anmol.vijaywargiya@go-jek.com>
2020-11-19 16:38:33 +0530

    Error reporting async

--- src/ziggurat/new_relic.clj
+++ src/ziggurat/new_relic.clj
@@ -1,6 +1,7 @@
 (ns ziggurat.new-relic
   (:require [new-reliquary.core :as newrelic]
-            [ziggurat.config :refer [ziggurat-config]])
+            [ziggurat.config :refer [ziggurat-config]]
+            [clojure.tools.logging :as log])
   (:import (java.util HashMap)
            [com.newrelic.api.agent NewRelic]))
 
@@ -9,7 +10,12 @@
      ~category ~transaction-name (^{:once true} fn* [] ~@body)))
 
 (defn- notice-error [^Throwable throwable message]
-  (NewRelic/noticeError throwable (HashMap. {"error_message" message}) false))
+  (let [^Runnable task #(try
+                          (with-tracing "reporting" "Error Reported"
+                            (NewRelic/noticeError throwable (HashMap. {"error_message" message}) false))
+                          (catch Exception e
+                            (log/warn e "Error while reporting error to new-relic")))]
+    (.start (Thread. task))))
 
 (defn report-error [throwable message]
   (when (get-in (ziggurat-config) [:new-relic :report-errors])
