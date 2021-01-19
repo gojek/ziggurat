1762d833147829d7fbe54e5dc2c31592e241bf79
prateek.khatri <prateek.khatri@go-jek.com>
2021-01-19 15:03:49 +0530

    trying lein version 2.9.1

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
+;; allow insecure downloads
+(require 'cemerick.pomegranate.aether)
+(cemerick.pomegranate.aether/register-wagon-factory!
+  "http" #(org.apache.maven.wagon.providers.http.HttpWagon.))
 (defproject tech.gojek/ziggurat "3.7.0"
   :description "A stream processing framework to build stateless applications on kafka"
   :url "https://github.com/gojektech/ziggurat"
