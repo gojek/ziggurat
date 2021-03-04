9850dcbf9705f61926b960fefd412ec6511489cd
anmol1vw13 <anmolvw07@gmail.com>
2021-03-04 15:30:32 +0530

    Implementation of auto commit feature for batch consumers (#208)
    * enables auto-commit
    
    * [Anmol|Hari] Implementation of the auto-commit feature for batch consumers
    
    * [Anmol|Hari] Removal of manual commit code from batch consumers
    
    * [Anmol|Hari] Linting issues fixed
    
    * [Anmol|Hari] Removal of tests related to commitSync for batch consumers
    
    * enables auto-commit
    
    * [Anmol|Shubhang] Removal of wakeup exception log
    
    * [Anmol|Shubhang] commit-interval-ms added to config.test.ci.edn
    
    * Uncommenting WakeupException Log
    
    * [Anmol] Putting in enable-auto-commit inside default-consumer-config
    
    Co-authored-by: shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
    Co-authored-by: Anmol Vijaywargiya <anmol.vijaywargiya@go-jek.com>

--- src/ziggurat/kafka_consumer/consumer.clj
+++ src/ziggurat/kafka_consumer/consumer.clj
@@ -1,28 +1,43 @@
 (ns ziggurat.kafka-consumer.consumer
   (:require [clojure.tools.logging :as log]
-            [ziggurat.kafka-consumer.consumer-handler :refer :all])
+            [ziggurat.kafka-consumer.consumer-handler :refer :all]
+            [ziggurat.util.map :as umap])
   (:import (java.util Map Properties)
            (org.apache.kafka.clients.consumer KafkaConsumer ConsumerConfig)
            (java.util.regex Pattern)))
 
-(defn- build-consumer-properties-map
-  [consumer-group-config]
-  (doto (Properties.)
-    (.putAll {ConsumerConfig/BOOTSTRAP_SERVERS_CONFIG        (:bootstrap-servers consumer-group-config)
-              ConsumerConfig/GROUP_ID_CONFIG                 (:consumer-group-id consumer-group-config)
-              ConsumerConfig/MAX_POLL_RECORDS_CONFIG         (int (or (:max-poll-records  consumer-group-config) 500))
-              ConsumerConfig/SESSION_TIMEOUT_MS_CONFIG       (int (or (:session-timeout-ms-config  consumer-group-config) 60000))
-              ConsumerConfig/ENABLE_AUTO_COMMIT_CONFIG       false
-              ConsumerConfig/KEY_DESERIALIZER_CLASS_CONFIG   (or (:key-deserializer-class-config consumer-group-config)
-                                                                 "org.apache.kafka.common.serialization.ByteArrayDeserializer")
-              ConsumerConfig/VALUE_DESERIALIZER_CLASS_CONFIG (or (:value-deserializer-class-config consumer-group-config)
-                                                                 "org.apache.kafka.common.serialization.ByteArrayDeserializer")})))
+(def default-consumer-config
+  {:commit-interval-ms 15000
+   :max-poll-records 500
+   :session-timeout-ms-config 60000
+   :enable-auto-commit true
+   :key-deserializer-class-config "org.apache.kafka.common.serialization.ByteArrayDeserializer"
+   :value-deserializer-class-config "org.apache.kafka.common.serialization.ByteArrayDeserializer"})
 
+(defn- build-consumer-properties-map
+  [{:keys [bootstrap-servers
+           consumer-group-id
+           max-poll-records
+           session-timeout-ms-config
+           commit-interval-ms
+           enable-auto-commit
+           key-deserializer-class-config
+           value-deserializer-class-config]}]
+  (doto (Properties.)
+    (.putAll {ConsumerConfig/BOOTSTRAP_SERVERS_CONFIG        bootstrap-servers
+              ConsumerConfig/GROUP_ID_CONFIG                 consumer-group-id
+              ConsumerConfig/MAX_POLL_RECORDS_CONFIG         (int max-poll-records)
+              ConsumerConfig/SESSION_TIMEOUT_MS_CONFIG       (int session-timeout-ms-config)
+              ConsumerConfig/ENABLE_AUTO_COMMIT_CONFIG       enable-auto-commit
+              ConsumerConfig/AUTO_COMMIT_INTERVAL_MS_CONFIG  (int commit-interval-ms)
+              ConsumerConfig/KEY_DESERIALIZER_CLASS_CONFIG   key-deserializer-class-config
+              ConsumerConfig/VALUE_DESERIALIZER_CLASS_CONFIG value-deserializer-class-config})))
 (defn create-consumer
   [topic-entity consumer-group-config]
   (try
-    (let [consumer (KafkaConsumer. ^Map (build-consumer-properties-map consumer-group-config))
-          topic-pattern (Pattern/compile (:origin-topic consumer-group-config))]
+    (let [merged-consumer-group-config (umap/deep-merge consumer-group-config default-consumer-config)
+          consumer (KafkaConsumer. ^Map (build-consumer-properties-map merged-consumer-group-config))
+          topic-pattern (Pattern/compile (:origin-topic merged-consumer-group-config))]
       (.subscribe consumer topic-pattern)
       consumer)
     (catch Exception e
--- test/ziggurat/kafka_consumer/consumer_test.clj
+++ test/ziggurat/kafka_consumer/consumer_test.clj
@@ -3,7 +3,8 @@
   (:require [ziggurat.kafka-consumer.consumer :refer [create-consumer]]
             [ziggurat.config :refer [ziggurat-config]]
             [ziggurat.fixtures :as fix])
-  (:import (org.apache.kafka.clients.consumer KafkaConsumer)))
+  (:import (org.apache.kafka.clients.consumer KafkaConsumer)
+           (java.util Properties)))
 
 (use-fixtures :once fix/mount-only-config)
 
@@ -14,6 +15,19 @@
       (is (contains? (set (keys (.listTopics consumer))) expected-origin-topic))
       (.unsubscribe consumer)
       (.close consumer)))
+  (testing "uses the config supplied by the user while creating the consumer"
+    (let [ziggurat-consumer-config (get-in (ziggurat-config) [:batch-routes :consumer-1])
+          consumer-properties (#'ziggurat.kafka-consumer.consumer/build-consumer-properties-map (assoc ziggurat-consumer-config :enable-auto-commit true))]
+      (with-redefs [ziggurat.kafka-consumer.consumer/build-consumer-properties-map (fn [consumer-config] (is (= (:commit-interval-ms consumer-config) (:commit-interval-ms ziggurat-consumer-config)))
+                                                                                     consumer-properties)]
+        (.close (create-consumer :consumer-1 ziggurat-consumer-config)))))
+  (testing "uses the default config for a specific property if the config supplied by the user does not have it"
+    (let [ziggurat-consumer-config (get-in (ziggurat-config) [:batch-routes :consumer-1])
+          consumer-config-without-commit-interval (dissoc ziggurat-consumer-config :commit-interval-ms)
+          consumer-properties (#'ziggurat.kafka-consumer.consumer/build-consumer-properties-map (assoc ziggurat-consumer-config :enable-auto-commit true))]
+      (with-redefs [ziggurat.kafka-consumer.consumer/build-consumer-properties-map (fn [consumer-config] (is (= (:commit-interval-ms consumer-config) (:commit-interval-ms ziggurat.kafka-consumer.consumer/default-consumer-config)))
+                                                                                     consumer-properties)]
+        (.close (create-consumer :consumer-1 consumer-config-without-commit-interval)))))
   (testing "returns nil when invalid configs are provided (KafkaConsumer throws Exception)"
     (let [consumer-config (get-in (ziggurat-config) [:batch-routes :consumer-1])]
       (is (= nil (create-consumer :consumer-1 (assoc-in consumer-config [:consumer-group-id] nil)))))))
