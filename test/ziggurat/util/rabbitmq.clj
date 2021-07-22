b20176f6076283600e31ecb7ea8ed22a7ba2058a
prateek.khatri <prateek.khatri@gojek.com>
2021-07-22 15:44:05 +0530

    fixing tests in mapper-test

--- test/ziggurat/util/rabbitmq.clj
+++ test/ziggurat/util/rabbitmq.clj
@@ -78,3 +78,8 @@
     (.close channel)
     (catch AlreadyClosedException _
       nil)))
+
+(defn bytes-to-str
+  [message-payload]
+  (-> message-payload
+      (update :message #(String. ^"[B" %))))
