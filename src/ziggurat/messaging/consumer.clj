a610eb8cdc73cdd47216cc9a34e2ff2548d10e05
prateek.khatri <prateek.khatri@gojek.com>
2022-09-23 18:11:42 +0530

    Starting and Stopping rabbitmq consumer connection with server. Also using it for deleting dead-set

--- src/ziggurat/messaging/consumer.clj
+++ src/ziggurat/messaging/consumer.clj
@@ -104,7 +104,7 @@
   "This method deletes `count` number of messages from RabbitMQ dead-letter queue for topic `topic-entity` and channel
   `channel`."
   [topic-entity channel count]
-  (with-open [ch (lch/open producer-connection)]
+  (with-open [ch (lch/open consumer-connection)]
     (let [queue-name (construct-queue-name topic-entity channel)]
       (doall (for [_ (range count)]
                (lb/get ch queue-name true))))))
