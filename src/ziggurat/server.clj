8fc6aa86e86b46d393515e800dd71e43f77537f9
shubhang.balkundi <shubhang.balkundi@go-jek.com>
2023-02-13 12:22:55 +0530

    makes graceful shutdown timeout configurable

--- src/ziggurat/server.clj
+++ src/ziggurat/server.clj
@@ -15,11 +15,14 @@
 (def default-stop-timeout-ms 30000)
 
 (defn configure-jetty [^Server server]
-  (let [stats-handler   (StatisticsHandler.)
-        default-handler (.getHandler server)]
+  (let [stats-handler       (StatisticsHandler.)
+        graceful-timeout-ms (get-in (ziggurat-config)
+                                    [:http-server :graceful-shutdown-timeout-ms]
+                                    default-stop-timeout-ms)
+        default-handler     (.getHandler server)]
     (.setHandler stats-handler default-handler)
     (.setHandler server stats-handler)
-    (.setStopTimeout server default-stop-timeout-ms)
+    (.setStopTimeout server graceful-timeout-ms)
     (.setStopAtShutdown server true)))
 
 (defn- start [handler]
