8f703761ef5d5ed0ce676616e853f10bf0f13f19
Abhinav Tiwary <abhinav.tiwary@gojek.com>
2024-03-21 15:59:29 +0530

    Added code to fetch key

--- src/ziggurat/header_transformer.clj
+++ src/ziggurat/header_transformer.clj
@@ -1,17 +1,17 @@
 (ns ziggurat.header-transformer
-  (:import [org.apache.kafka.streams.kstream ValueTransformer]
+  (:import [org.apache.kafka.streams.kstream ValueTransformerWithKey]
            [org.apache.kafka.streams.processor ProcessorContext]))
 
-(deftype HeaderTransformer [^{:volatile-mutable true} processor-context] ValueTransformer
+(deftype HeaderTransformer [^{:volatile-mutable true} processor-context] ValueTransformerWithKey
          (^void init [_ ^ProcessorContext context]
            (set! processor-context context))
-         (transform [_ record-value]
+         (transform [_ record-key record-value]
            (let [topic     (.topic processor-context)
                  timestamp (.timestamp processor-context)
                  partition (.partition processor-context)
                  headers   (.headers processor-context)
                  metadata  {:topic topic :timestamp timestamp :partition partition}]
-             {:value record-value :headers headers :metadata metadata}))
+             {:value record-value :headers headers :metadata metadata :key record-key}))
          (close [_] nil))
 
 (defn create []
