428bfdac4331e23bca9b49d4547924e728963710
vruttantmankad <vruttant.mankad@gojek.com>
2024-04-03 15:08:52 +0530

    fix lint

--- test/ziggurat/mapper_test.clj
+++ test/ziggurat/mapper_test.clj
@@ -323,15 +323,15 @@
           ((channel-mapper-func user-handler-fn channel)  message-payload)
           (is @user-handler-called))))
     (testing "retry-count set to nil returns rabbitmq-retry-count 0"
-         (let [user-handler-called (atom false)
-               message-payload {:retry-count 0
-                                :topic-entity topic
-                                :metadata     metadata}
-               user-handler-fn (fn [user-msg-payload]
-                                 (reset! user-handler-called true)
-                                 (is (= ((get user-msg-payload :metadata) :rabbitmq-retry-count ) 0)))]
-           (with-redefs [metrics/increment-count (constantly nil)
+      (let [user-handler-called (atom false)
+            message-payload {:retry-count 0
+                             :topic-entity topic
+                             :metadata     metadata}
+            user-handler-fn (fn [user-msg-payload]
+                              (reset! user-handler-called true)
+                              (is (= ((get user-msg-payload :metadata) :rabbitmq-retry-count ) 0)))]
+        (with-redefs [metrics/increment-count (constantly nil)
                          get-channel-retry-count (constantly nil) ]
-             ((channel-mapper-func user-handler-fn channel) message-payload)
-             (is @user-handler-called))))))
+          ((channel-mapper-func user-handler-fn channel) message-payload)
+          (is @user-handler-called))))))
 
